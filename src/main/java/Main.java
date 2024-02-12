e3141b2d7343c4fecf328275461e09b41de6722c
Arthur <guritch21@gmail.com>
2024-02-12 11:54:59 +0300

    added check code to Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,45 +1,80 @@
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        MyArrayList<Integer> list = new MyArrayList<>();
-        System.out.println(list);
-        list.add(531);
-        list.add(5435);
-        list.add(454);
-        System.out.println(list);
-        list.add(2,999);
-        System.out.println(list);
-        System.out.println(list.size());
-        System.out.println(list.delete(2) + " удален");
-        System.out.println(list);
-        System.out.println(list.size());
-        System.out.println(list.delete((Integer) 5435) + " удален");
-        System.out.println(list);
-        System.out.println(list.size());
+    public static <T> void main(String[] args) {
+        // Создаем лист Integer
+        MyArrayList<Integer> numbersList = new MyArrayList<>();
+        System.out.println(numbersList);
+
+        // Добавляем элементы в конец списка
+        numbersList.add(531);
+        numbersList.add(5435);
+        numbersList.add(454);
+        numbersList.add(1);
+        numbersList.add(3);
+        System.out.println(numbersList);
+
+        // Добавляем элемент по индексу
+        numbersList.add(2,999);
+        System.out.println(numbersList);
+        System.out.println(numbersList.size());
+
+        // Удаляем элемент по индексу
+        System.out.println(numbersList.delete(2) + " удален");
+        System.out.println(numbersList);
+        System.out.println(numbersList.size());
+
+        // Удаляем элемент по объекту
+        System.out.println(numbersList.delete((Integer) 5435) + " удален");
+        System.out.println(numbersList);
+        System.out.println(numbersList.size());
+
+        // Перезаписываем объект по индексу
+        numbersList.set(0, 4);
+        numbersList.set(1, 2);
+        System.out.println(numbersList);
+        System.out.println(numbersList.size());
         System.out.println();
 
+        // Создаем лист типа Person и добавляем объекты
         MyArrayList<Person> personList = new MyArrayList<>();
         personList.add(new Person("Bob", 20));
         personList.add(new Person("John", 35));
         System.out.println(personList);
+        personList.get(1).setAge(40);
+        System.out.println(personList);
+
+        // Вызываем метод contains с различными объектами
         Person otherPerson = new Person("Tom", 20);
         System.out.println(personList.contains(otherPerson));
-        Person otherBob = new Person("Bob", 20);
+        Person bob = new Person("Bob", 20);
+        System.out.println(personList.contains(bob));
+        Person otherBob = new Person("Bob", 60);
         System.out.println(personList.contains(otherBob));
-        Person oldBob = new Person("Bob", 60);
-        System.out.println(personList.contains(oldBob));
-        if (personList.get(1).getClass() == otherBob.getClass()) {
-            System.out.println("Class equals");
-        }
-        System.out.println();
 
+        // Создаем список из множества
         Set<String> stringSet = new HashSet<>(List.of("первый", "второй", "третий"));
         MyArrayList<String> myArrayListFromSet = new MyArrayList<>(stringSet);
         System.out.println(myArrayListFromSet);
+
+        // Полностью очищаем список
         myArrayListFromSet.clear();
         System.out.println(myArrayListFromSet);
+
+        // Сортируем список numbersList через интерфейс Comparable (у Integer по умолчанию по возрастанию)
+        System.out.println(numbersList);
+        numbersList.sort();
+        System.out.println(numbersList);
+
+        // Сортируем список numbersList через интерфейс Comparator (реализуем свой вариант по убыванию, меняем местами -1 и 1 из оригинального кода)
+        numbersList.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return (o1 < o2) ? 1 : ((o1 == o2) ? 0 : -1);
+            }
+        });
+        System.out.println(numbersList);
+
+        personList.sort();
     }
 }
